2ff5d6efca4900853fe422cf206570c0b195ee0c
Justin Chrisleigh M. Alqueza <chrisleighjustin@gmail.com>
2026-04-27 09:35:28 +0800

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,7 +1,11 @@
 public class Weapon {
     String name;
+    int attackBonus;
+    int manaBonus; 
 
-    public Weapon(String name) {
+    public Weapon(String name, int attackBonus, int manaBonus) {
         this.name = name;
+        this.attackBonus = attackBonus;
+        this.manaBonus = manaBonus;
     }
 }
